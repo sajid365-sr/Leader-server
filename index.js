0bf4ff1e4950c26eab8e4b4d097aa30965de0f78
sajid365-sr <sajidsorker86@gmail.com>
2022-10-31 04:23:35 +0600

    dynamic api created for single course

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 5000;
 
 const course = require('./data.json');
+app.use(cors());
 
 app.get('/', (req, res) =>{
     res.send(course);
 })
+
+app.get('/course/:id', (req, res) =>{
+    const id = req.params.id;
+    const detailCourse = course.find(one => one.id === id);
+    res.send(detailCourse);
+})
+
 app.listen(port, ()=>{
     console.log('Server is running on the port: ', port);
 })
